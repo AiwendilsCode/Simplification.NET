499e8be52a7d1bf57cb2892445cf15973ff8b8b3
AiwendilsCode <80208573+AiwendilsCode@users.noreply.github.com>
2024-06-05 17:38:01 +0200

    Update README.md and example program.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Simplification
+C# wrapper for [Rust library](https://github.com/urschrei/rdp/tree/master) by [urschrei](https://github.com/urschrei) for [Ramer-Douglas-Peucker](https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm) and [Visvalingam-Whyatt](https://en.wikipedia.org/wiki/Visvalingam%E2%80%93Whyatt_algorithm) algorithms.
+
+## Usage
+
+```
+using Simplification.Algorithms;
+
+double[][] input = [
+    [0.0, 0.0],
+    [5.0, 4.0],
+    [11.0, 5.5],
+    [17.3, 3.2],
+    [27.8, 0.1],
+];
+
+ISimplificationAlgorithm rdpAlgorithm = new RdpAlgorithm();
+
+// For RDP, Try an epsilon of 1.0 to start with. Other sensible values include 0.01, 0.001
+double[][] simplified = rdpAlgorithm.Simplify(input, 1.0); // returns simplified data
+UIntPtr[] simplifiedIndices = rdpAlgorithm.SimplifyIdx(input, 1.0); // returns simplified indices
+
+ISimplificationAlgorithm visvAlgorithm = new VisvalingamWhyattAlgorithm();
+
+simplified = visvAlgorithm.Simplify(input, 1.0); // returns simplified data
+simplifiedIndices = visvAlgorithm.SimplifyIdx(input, 1.0); // returns simplified indices
+
+simplified = ((VisvalingamWhyattAlgorithm)visvAlgorithm)
+    .PreserveTopologySimplify(input, 1.0); // returns simplified data, while preserving topology
+```
--- src/Simplification.CmdTests/Program.cs
+++ src/Simplification.CmdTests/Program.cs
@@ -1,17 +1,23 @@
 ﻿using Simplification.Algorithms;
-using System.Globalization;
 
-string[] lines = File.ReadAllLines("generatedValuesSin.csv");
+double[][] input = [
+    [0.0, 0.0],
+    [5.0, 4.0],
+    [11.0, 5.5],
+    [17.3, 3.2],
+    [27.8, 0.1],
+];
 
-double[][] data = new double[1500][];
+ISimplificationAlgorithm rdpAlgorithm = new RdpAlgorithm();
 
-for (int i = 1; i < 1501; i++) // skip first line
-{
-    data[i - 1] = [(i - 1) * 3600, double.Parse(lines[i], NumberStyles.AllowExponent | NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture)];
-}
+// For RDP, Try an epsilon of 1.0 to start with. Other sensible values include 0.01, 0.001
+double[][] simplified = rdpAlgorithm.Simplify(input, 1.0); // returns simplified data
+UIntPtr[] simplifiedIndices = rdpAlgorithm.SimplifyIdx(input, 1.0); // returns simplified indices
 
-ISimplificationAlgorithm algorithm = new RdpAlgorithm();
+ISimplificationAlgorithm visvAlgorithm = new VisvalingamWhyattAlgorithm();
 
-UIntPtr[] _ = algorithm.SimplifyIdx(data, 0.005);
+simplified = visvAlgorithm.Simplify(input, 1.0); // returns simplified data
+simplifiedIndices = visvAlgorithm.SimplifyIdx(input, 1.0); // returns simplified indices
 
-;
+simplified = ((VisvalingamWhyattAlgorithm)visvAlgorithm)
+    .PreserveTopologySimplify(input, 1.0); // returns simplified data, while preserving topology
